81b6b71b173a0ac7382e834c68a4e9c9846b63ae
Eric Khodorenko <ekhodor8@gmail.com>
2024-07-22 09:23:42 -0700

    added posts and individual post page url/views

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("This Works!")
+def posts(request):
+    return HttpResponse("This works too!")
+
+def post(request, post):
+    
+    return HttpResponse("Individual blog post works as well!")
 
 
 # Create your views here.
