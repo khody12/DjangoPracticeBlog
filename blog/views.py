95f806e2037076b29e4eeb5da3702466fd5fae68
Eric Khodorenko <ekhodor8@gmail.com>
2024-07-24 16:06:22 -0700

    major css changes and base website features completed

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils.text import slugify 
+from datetime import date
 
 list_of_posts = [
-  {"name": "Mountain Hiking", "heading":"There's nothing like the views you get when hiking in the mountains", "content":"yadayadayada", },
-  {"name": "Programming is great", "heading": "Did you ever spend hours searching that one error in your code", "content": "yadayada"},
-  {"name": "Nature at its best","heading":"Nature is amazing!","content":"yadayadayada" }
+  {"name": "Mountain Hiking", "heading":"There's nothing like the views you get when hiking in the mountains", "story":"When i was hiking i saw all sorts of animals and plants", "date":date(2021,7,21), "image": "woods.jpg"},
+  {"name": "Programming is great", "heading": "Did you ever spend hours searching that one error in your code", "story": "yadayada", "date": date(2021,8,5), "image":"coding.jpg"},
+  {"name": "Nature at its best","heading":"Nature is amazing!","story":"yadayadayada", "date": date(2022,1,10),"image":"mountains.jpg"}
 ]
 latest_posts = list_of_posts
+
+def get_date(post):
+    return post['date']
+
+    
 
 
 for post in list_of_posts: # i am using the names in the dictionary here as the urls for our posts, but i have to slugify them with dashes "-" between the words. in a bigger program this would be a function.
@@ -21,9 +27,12 @@ context = {
 
 
 def index(request):
-    return render(request, "blog/index.html", context)
+    sorted_posts = sorted(list_of_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {"posts":latest_posts})
 
 def posts(request):
+    
     
     
     return render(request, "blog/posts.html", {"posts": list_of_posts})
