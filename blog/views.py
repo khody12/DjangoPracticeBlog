1b97b341b94a2b2fb037b2bdb34eabee89758e7e
khody <ekhodor8@gmail.com>
2024-07-28 20:06:40 -0700

    minor changes to views implementing post sorting

--- blog/views.py
+++ blog/views.py
@@ -7,23 +7,25 @@ from .models import Author, Post, Tag
 
 
 
-listOfPosts = Post.objects.all()
+list_of_posts = Post.objects.all()
 
 
 # because render requires a dictionary to be passed in, we first have to make a dictionary with a key being how we access the list within the html. 
 
 
 def index(request):
+    latest_posts = Post.objects.all().order_by("-date")[:3] # this gets the 3 most recent posts, and also
+
     
-    return render(request, "blog/index.html", {"posts":listOfPosts})
+    return render(request, "blog/index.html", {"posts":latest_posts})
 
 def posts(request):
     
-    return render(request, "blog/posts.html", {"posts": listOfPosts})
+    return render(request, "blog/posts.html", {"posts": list_of_posts})
 
 def post(request, postName):
 
-    post = listOfPosts.get(slug=postName)
+    post = list_of_posts.get(slug=postName)
 
     return render(request, "blog/post.html", {"post":post})
 
